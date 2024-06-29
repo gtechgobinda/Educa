f23ee7a6c054612099ec97259bd3bfbbca490f81
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-06-29 14:55:27 +0530

    done project set

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,16 +4,9 @@ import { Stack, Tabs } from "expo-router";
 
 const RootLayout = () => {
   return (
-    // <Stack>
-    //   <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    // </Stack>
-    // <Tabs/>
-    <Tabs>
-    <Tabs.Screen name="course" options={{ headerShown: false }}/>
-    <Tabs.Screen name="college" options={{ headerShown: false }}/>
-    <Tabs.Screen name="liveClass" options={{ headerShown: false }}/>
-    <Tabs.Screen name="question" options={{ headerShown: false }}/>
-  </Tabs>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { Link } from "expo-router";
+import React from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 const Home = () => {
   return (
-    // <View>
-    //   <Text>index</Text>
-    // </View>
-    <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-  </Stack>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.title}>HELLO EDUCA</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+      <Link href="/course" style={styles.btn}>
+        <Text style={styles.text}>LOGIN</Text>
+      </Link>
+    </View>
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  btn: {
+    height: 40,
+    backgroundColor: "#d4d4d4",
+    color: "#000000",
+    textAlign: "center",
+    fontSize: 20,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Home;
